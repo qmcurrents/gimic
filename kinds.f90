e9bd3d702d8d2d4e76d3d8681e5710c2f52d4262
Jonas Juselius <jonas.juselius@uit.no>
2009-05-19 15:15:04 +0200

    Removed the quad precision crap.

--- kinds.f90
+++ kinds.f90
@@ -4,8 +4,8 @@ module kinds_m
 	integer, parameter :: SP = kind(0.0e0)
 	integer, parameter :: DP = kind(0.0d0)
 #ifdef HAVE_QP
- 	integer, parameter :: QP = kind(0.0q0)
-	integer, parameter :: QP = 16
+! 	integer, parameter :: QP = kind(0.0q0)
+!	integer, parameter :: QP = 16
 #endif
 	integer, parameter :: I2 = 2
 	integer, parameter :: I4 = 4
