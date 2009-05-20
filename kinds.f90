4a1908d56ce0af764ce4a6aa3dac3d701a8ffc28
Jonas Juselius <jonas.juselius@uit.no>
2009-05-20 13:01:11 +0200

    Now QP is TOTALLY removed from kinds.f90

--- kinds.f90
+++ kinds.f90
@@ -3,10 +3,7 @@ module kinds_m
 	integer, parameter :: MP = kind(0)
 	integer, parameter :: SP = kind(0.0e0)
 	integer, parameter :: DP = kind(0.0d0)
-#ifdef HAVE_QP
-! 	integer, parameter :: QP = kind(0.0q0)
-!	integer, parameter :: QP = 16
-#endif
+
 	integer, parameter :: I2 = 2
 	integer, parameter :: I4 = 4
 	integer, parameter :: I8 = 8
