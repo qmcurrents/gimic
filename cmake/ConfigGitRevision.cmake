9452f670d005d3a425ca74e365b3d800fb3bd537
Jonas Juselius <jonas.juselius@uit.no>
2012-01-27 09:45:03 +0100

    Fixed small cmake bug.

--- cmake/ConfigGitRevision.cmake
+++ cmake/ConfigGitRevision.cmake
@@ -4,6 +4,7 @@ if (GIT_FOUND)
 	execute_process(
 		COMMAND ${GIT_EXECUTABLE} rev-list --abbrev-commit --max-count=1 HEAD
 		OUTPUT_VARIABLE GIT_REVISION
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 		)
 	string(STRIP
 		${GIT_REVISION}
