51d97f51ffbbbc28624bd87b8b39e9c10e9ce1b5
Heike Fliegl <heike@stallo-2.local>
2012-04-25 17:20:25 +0200

    added magnet.py

---
+++ src/pygimic/magnet.py
@@ -0,0 +1,14 @@
+class Magnet:
+    def __init__(self, grid, b):
+        self.grid = grid
+        self.mag = b
+
+    def __str__(self):
+        return str(self.grid) + ' ' + str(self.mag)
+
+
+
+
+    
+
+# vim:et:sw=4:ts=4
