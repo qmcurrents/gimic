f6bf10a0da5d2a17c0bee69be2094c314e9926a9
Jonas Juselius <jonas.juselius@uit.no>
2012-05-01 15:02:17 +0200

    Functional fields: Scalar, Vector and Tensor fields.

--- src/pygimic/field.py
+++ src/pygimic/field.py
@@ -1,67 +1,109 @@
 import numpy as np
 from copy import deepcopy
-from grid import Grid
-from magnet import Magnet
+from grid import Grid, GridIterator
+from gexceptions import NotReached
 
-class Field:
+class Field(GridIterator):
     def __init__(self, grid):
+        GridIterator.__init__(self)
         self.grid = grid
-        self.field = []
-        self.it = 0
-        self.iteridx = [0, 0, 0]
+        self.npts = grid.size()
 
     def calc(self, func):
-        for r in self.grid:
-            self.field.append(func(r))
+        raise NotReached('Baseclass method calc()')
 
-    def __getitem__(self, i, j, k=0):
-        return self.get(i, j, k)
+    def __getitem__(self, idx):
+        return self.getitem(idx)
 
-    def get(self, i, j, k=0):
-        n = grid.size()
-        return self.field[i + n[0] * j + n[0] * n[1] * k]
+class ScalarField(Field):
+    def __init__(self, grid, dtype=float):
+        Field.__init__(self, grid)
+        self.field = np.ndarray(self.grid.size(), dtype=dtype)
 
-    def __iter__(self):
-        return self
+    def getitem(self, idx):
+        return self.field[idx[0], idx[1], idx[2]]
 
-    def next(self):
-        n = self.it
-        if self.it < len(self.field):
-            self.it += 1
+    itervalue = getitem
+
+    def calc(self, func):
+        for i, j, k in self.grid.range():
+            r = self.grid.gridpoint((i, j, k))
+            self.field[i, j, k] = func(r)
+
+    def get(self, k=None):
+        if k:
+            return self.field[:, :, k]
         else:
-            self.it = 0
-            raise StopIteration()
-        return self.field[n]
+            return self.field
 
-    def fieldgen(self):
-        npts = self.grid.size()
-        for i in self.field:
-            cur = deepcopy(self.iteridx)
-            self.iteridx[0] += 1
-            if self.iteridx[0] == npts[0]:
-                self.iteridx[0] = 0
-                self.iteridx[1] += 1
-                if self.iteridx[1] == npts[1]:
-                    self.iteridx[0] = 0
-                    self.iteridx[1] = 0
-                    self.iteridx[2] += 1
-                    if self.iteridx[2] == npts[2]:
-                        self.iteridx = [0, 0, 0]
-            yield self.grid.point(cur), self.get(cur[0], cur[1], cur[2])
+class VectorField(Field):
+    dim = 3
+    def __init__(self, grid, dtype=float):
+        Field.__init__(self, grid)
+        self.field = []
+        for i in range(self.dim):
+            self.field.append(np.ndarray(self.grid.size(), dtype=dtype))
 
+    def getitem(self, idx):
+        v = np.zeros(self.dim)
+        for i in range(self.dim):
+            v[i] = self.field[i][idx[0], idx[1], idx[2]]
+        return v
+
+    itervalue = getitem
+
+    def calc(self, func):
+        for i, j, k in self.grid.range():
+            r = self.grid.gridpoint((i, j, k))
+            v = func(r)
+            for n in range(self.dim):
+                self.field[n][i, j, k] = v[n]
+
+    def get(self, k=None):
+        v = []
+        if k:
+            for i in range(self.dim):
+                v[i] = self.field[i][:, :, k]
+            return v
+        else:
+            return self.field
+
+class TensorField(VectorField):
+    dim = 9
+    def __init__(self, grid, dtype=float):
+        VectorField.__init__(self, grid, dtype)
 
 if __name__ == '__main__':
-    q=-1.0
+    s=-1.0
+    v=np.zeros(3)
+    t=np.zeros(9)
 
-    def ftest(r):
-        global q
-        q += 1.0 
-        return q
+    def stest(r):
+        global s
+        s += 1.0 
+        return s
+
+    def vtest(r):
+        global v
+        v += 1.0 
+        return v
+
+    def ttest(r):
+        global t
+        t += 1.0 
+        return t
 
     grid = Grid((2.0, 2.0, 2.0), npts=4)
-    f = Field(grid)
-    f.calc(ftest)
-    for i in f.fieldgen():
+    f = ScalarField(grid)
+    f.calc(stest)
+    for i in f:
         print i
+    print f.get()
+
+#    f = VectorField(grid)
+#    f.calc(vtest)
+#    for i in f:
+#        print i
+#    print f.get()
 
 # vim:et:ts=4:sw=4
