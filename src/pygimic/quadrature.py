af9c643da76481d90416108d7207007a7537e15b
Jonas Juselius <jonas.juselius@uit.no>
2012-05-01 14:52:23 +0200

    Stub quadrature (incomplete).
    Moved Gauss-Legendre (stub) generation to quadrature.

--- src/pygimic/quadrature.py
+++ src/pygimic/quadrature.py
@@ -1,10 +1,17 @@
 import numpy as np
 from grid import Grid
 from gauss_legendre import GaussLegendre
+from gexecptions import NotImplemented
+
+class GaussLegendre:
+    def __init__(self, order):
+        raise NotImplemented(GaussLegendre)
 
 class GridQuadrature:
     def __init__(self, grid, func):
         self.grid = grid
         self.func = func
+        raise NotImplemented(GridQuadrature)
+
 
 # vim:et:ts=4:sw=4
