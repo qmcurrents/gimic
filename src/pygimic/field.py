Fixed grid weighting and radius.

src/pygimic/field.py
@@ -35,7 +35,12 @@ class ScalarField(Field):
     def calc(self, func):
         for i, j, k in self.grid.range():
             r = self.grid.gridpoint((i, j, k))
-            self.field[i, j, k] = func(r)
+            w = self.grid.gridweight((i, j, k))
+            if abs(w) > 0.0:
+                self.field[i, j, k] = func(r)
+            else:
+                self.field[i, j, k] = 0.0
+
 
     def get(self, k=None):
         if k is not None:
@@ -63,12 +68,10 @@ class VectorField(Field):
         for i, j, k in self.grid.range():
             r = self.grid.gridpoint((i, j, k))
             w = self.grid.gridweight((i, j, k))
-            if w == 0.0:
-                v = np.zeros(self.dim)
-            else:
+            if abs(w) > 0.0:
                 v = func(r) 
-            for n in range(self.dim):
-                self.field[n][i, j, k] = v[n] * w
+            else:
+                v = np.zeros(self.dim)
 
     def get(self, k=None):
         v = []

src/pygimic/grid.py
@@ -286,18 +286,19 @@ class Grid(GridIterator):
     def gridpoint(self, r):
         i, j, k = r
         return np.array((self.axes[0][i], self.axes[1][j], self.axes[2][k]))
-#        return self.axes[0][i] * self.basis[:, 0] + \
-#            self.axes[1][j] * self.basis[:, 1] + \
-#            self.axes[2][k] * self.basis[:, 2] 
+
+    # Definition for the iterator base class
+    itervalue = gridpoint
 
     def gridweight(self, r):
         i, j, k = r
         return self.axes[0].get_weigth(i) * \
                 self.axes[1].get_weigth(j) * self.axes[2].get_weigth(k)
-#        return np.array((self.axes[0].get_weigth(i),
-#                self.axes[1].get_weigth(j), self.axes[2].get_weigth(k)))
-    # Definition for the iterator base class
-    itervalue = gridpoint
+
+    def gridweights(self, r):
+        i, j, k = r
+        return np.array((self.axes[0].get_weigth(i),
+                self.axes[1].get_weigth(j), self.axes[2].get_weigth(k)))
 
     def get_normal(self):
         return self.get_k()