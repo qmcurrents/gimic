68947b3c09f83c2039f06843df414e4f112d8c7c
mariavd <maria.dimitrova@helsinki.fi>
2016-12-20 16:52:54 +0200

    Small fixed for the Gimic testing scripts

--- test/benzene/int/gimic-test.sh
+++ test/benzene/int/gimic-test.sh
@@ -18,11 +18,18 @@ dia_ref=$(grep -A 2 "Induced current" gimic.out | awk '{ if (NR == 2) printf("% 
 para_ref=$(grep -A 2 "Induced current" gimic.out | awk '{ if (NR == 3) printf("% f\n", $5); }')
 total_ref=$(grep "Induced current (nA/T)" gimic.out | awk '{printf("% f\n", $5); }')
 
+# avoid returning success value by default
+test1=1
+test2=1
+test3=1
+
 test1=$( awk -v diatropic="$diatropic" -v dia_ref="$dia_ref" 'BEGIN{ if ((dia_ref - diatropic) < 1e-5) { print 0; } }'  )
 test2=$( awk -v paratropic="$paratropic" -v para_ref="$para_ref" 'BEGIN{ if ((para_ref - paratropic) < 1e-5) { print 0; } }' )
 test3=$( awk -v total="$total" -v total_ref="$total_ref" 'BEGIN{ if ((total_ref - total) < 1e-5) { print 0; } }' )
 
 if [ $test1 -eq 0 ] && [ $test2 -eq 0 ] && [ $test3 -eq 0 ]
 then
-    echo "success"
+    echo "0" # success
+else
+    echo "1" # failure
 fi
