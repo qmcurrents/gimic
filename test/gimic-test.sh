29d1e1c8e60ba645fbf4a105dc761bd912895766
mariavd <maria.dimitrova@helsinki.fi>
2017-01-09 13:53:57 +0200

    The gimic-test.sh script runs from the test directory rather than from ./test/benzene/

--- test/gimic-test.sh
+++ test/gimic-test.sh
@@ -8,27 +8,40 @@ then
     exit
 fi
 
-cp ../XDENS XDENS
-cp ../MOL MOL
+testname=benzene
 
-$gimicdir/gimic gimic.inp > gimic.test.out 
+printf "\nPerforming test on $testname bond integral\n"
 
-diatropic=$(grep -A 2 "Induced current" gimic.test.out | awk '{ if (NR == 2) printf("% f\n", $5); }')
-paratropic=$(grep -A 2 "Induced current" gimic.test.out | awk '{ if (NR == 3) printf("% f\n", $5); }')
-total=$(grep "Induced current (nA/T)" gimic.test.out | awk '{printf("% f\n", $5); }')
+cp ./$testname/XDENS ./$testname/int/XDENS
+cp ./$testname/MOL ./$testname/int/MOL
 
-dia_ref=$(grep -A 2 "Induced current" gimic.out | awk '{ if (NR == 2) printf("% f\n", $5); }')
-para_ref=$(grep -A 2 "Induced current" gimic.out | awk '{ if (NR == 3) printf("% f\n", $5); }')
-total_ref=$(grep "Induced current (nA/T)" gimic.out | awk '{printf("% f\n", $5); }')
+(cd ./$testname/int && $gimicdir/gimic gimic.inp > gimic.test.out )
+
+diatropic=$(grep -A 2 "Induced current" ./$testname/int/gimic.test.out | awk '{ if (NR == 2) printf("% f\n", $5); }')
+paratropic=$(grep -A 2 "Induced current" ./$testname/int/gimic.test.out | awk '{ if (NR == 3) printf("% f\n", $5); }')
+total=$(grep "Induced current (nA/T)" ./$testname/int/gimic.test.out | awk '{printf("% f\n", $5); }')
+
+# Debugging
+echo $diatropic $paratropic $total
+
+dia_ref=$(grep -A 2 "Induced current" ./$testname/int/gimic.out | awk '{ if (NR == 2) printf("% f\n", $5); }')
+para_ref=$(grep -A 2 "Induced current" ./$testname/int/gimic.out | awk '{ if (NR == 3) printf("% f\n", $5); }')
+total_ref=$(grep "Induced current (nA/T)" ./$testname/int/gimic.out | awk '{printf("% f\n", $5); }')
+
+# Debugging
+echo $dia_ref $para_ref $total_ref
 
 # avoid returning success value by default
-test1=1
-test2=1
-test3=1
+test1=2
+test2=2
+test3=2
 
-test1=$( awk -v diatropic="$diatropic" -v dia_ref="$dia_ref" 'BEGIN{ if ((dia_ref - diatropic) < 1e-5) { print 0; } }'  )
-test2=$( awk -v paratropic="$paratropic" -v para_ref="$para_ref" 'BEGIN{ if ((para_ref - paratropic) < 1e-5) { print 0; } }' )
-test3=$( awk -v total="$total" -v total_ref="$total_ref" 'BEGIN{ if ((total_ref - total) < 1e-5) { print 0; } }' )
+test1=$( awk -v diatropic="$diatropic" -v dia_ref="$dia_ref" 'BEGIN{ if ((dia_ref - diatropic) < 1e-5) { print 0; } else {print 1; } }'  )
+test2=$( awk -v paratropic="$paratropic" -v para_ref="$para_ref" 'BEGIN{ if ((para_ref - paratropic) < 1e-5) { print 0; } else {print 1; } }' )
+test3=$( awk -v total="$total" -v total_ref="$total_ref" 'BEGIN{ if ((total_ref - total) < 1e-5) { print 0; } else {print 1; } }' )
+
+#Debugging
+echo $test1 $test2 $test3
 
 if [ $test1 -eq 0 ] && [ $test2 -eq 0 ] && [ $test3 -eq 0 ]
 then
@@ -37,4 +50,4 @@ else
     echo "1" # failure
 fi
 
-rm -rf XDENS MOL
+rm -rf ./$testname/int/XDENS ./$testname/int/MOL
