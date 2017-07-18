886131d5f5e01ba4e2d038ad60fc2a7121182599
Radovan Bast <bast@users.noreply.github.com>
2017-07-18 16:54:37 +0200

    refactor

--- cmake/custom/own.cmake
+++ cmake/custom/own.cmake
@@ -1,19 +1,17 @@
 set(CMAKE_INSTALL_PREFIX ${PROJECT_BINARY_DIR})
 
-unset(CMAKE_LIBRARY_ARCHITECTURE)
-
 include(GNUInstallDirs)
-include(ConfigProjectVersion)
-include(ConfigGitRevision)
+
+include(version)
+include(git)
 
 set(PYTHON_SITE_INSTALL_DIR
     lib/python${PYTHON_VERSION}/site-packages/gimic)
-include(UseCython)
-include(ConfigTesting)
 
-set(CMAKE_Fortran_MODULE_DIRECTORY ${CMAKE_BINARY_DIR}/modules)
+include(cython)
+include(testing)
 
-configure_file (
+configure_file(
     ${PROJECT_SOURCE_DIR}/config.h.in
     ${PROJECT_BINARY_DIR}/config.h
     )
