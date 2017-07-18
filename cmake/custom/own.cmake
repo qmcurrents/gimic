8a910f12a7c2dd333f41526ac9bdb33cc5705fdd
Radovan Bast <bast@users.noreply.github.com>
2017-07-18 16:40:45 +0200

    update CMakeLists.txt

---
+++ cmake/custom/own.cmake
@@ -0,0 +1,26 @@
+set(CMAKE_INSTALL_PREFIX ${PROJECT_BINARY_DIR})
+
+unset (CMAKE_LIBRARY_ARCHITECTURE)
+include(GNUInstallDirs)
+include(ConfigProjectVersion)
+include(ConfigSafeGuards)
+include(ConfigGitRevision)
+include(ConfigCompilerFlags)
+
+find_package(Python REQUIRED)
+set(PYTHON_SITE_INSTALL_DIR
+    lib/python${PYTHON_VERSION}/site-packages/gimic)
+include(UseCython)
+include(ConfigTesting)
+include(ConfigPackaging)
+
+set(CMAKE_Fortran_MODULE_DIRECTORY ${CMAKE_BINARY_DIR}/modules)
+
+configure_file (
+    ${PROJECT_SOURCE_DIR}/config.h.in
+    ${PROJECT_BINARY_DIR}/config.h
+    )
+
+add_subdirectory(src)
+add_subdirectory(tools)
+add_subdirectory(test)
