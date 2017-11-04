cbc8ff3d60f479f63cc1aeea39388d33250c688c
mariavd <maria.dimitrova@helsinki.fi>
2017-11-04 16:39:05 +0200

    Updated the suggested bash aliases

--- jobscripts/setup.sh
+++ jobscripts/setup.sh
@@ -28,20 +28,27 @@ do
     fi
 done
 
-echo; echo "USEFUL ALIASES:"
+echo; echo "USEFUL ALIASES AND FUNCTIONS:"; 
+echo "_________________________________________"; echo
 
-echo ' export GIMIC_HOME='${SCRIPTS_DIR/%jobscripts}
+echo 'export GIMIC_HOME='${SCRIPTS_DIR/%jobscripts}
 
 echo
 
-echo ' alias gim="$GIMIC_HOME/jobscripts/gimic-run.sh" '
-echo ' alias gcurrent="$GIMIC_HOME/jobscripts/current-profile.sh" '
-echo ' alias gmol="python $GIMIC_HOME/jobscripts/turbo2gimic.py > MOL" '
-echo ' alias grid="xmakemol -f grid.xyz &" '
-echo ' alias dgrid="gimic --dryrun bond_integral.inp > /dev/null && xmakemol -f grid.xyz &" '
-echo ' alias plotcurrent=" $GIMIC_HOME/jobscripts/plot-current-profile.sh" '
-echo ' alias revplotcurrent="gnuplot $GIMIC_HOME/jobscripts/current-profile-plot-reverse.gnu" '
-echo ' alias gpng="file=$(ls *para.eps) && convert -density 300 $file -resize 1024x1024 $file.png" '
-echo ' alias gsq="$GIMIC_HOME/jobscripts/squares-profile.sh" '
-echo ' alias geps="display *eps &" '
-echo ' alias intprofile="$GIMIC_HOME/jobscripts/intprofile.sh" '
+echo 'python $GIMIC_HOME/install/bin/turbo2gimic.py > MOL; '
+echo 'alias gim="$GIMIC_HOME/jobscripts/gimic-run.sh" '
+echo 'alias gcurrent="$GIMIC_HOME/jobscripts/current-profile.sh" '
+echo 'alias gmol="python $GIMIC_HOME/jobscripts/turbo2gimic.py > MOL" '
+echo 'alias grid="xmakemol -f grid.xyz &" '
+echo 'alias dgrid="gimic --dryrun bond_integral.inp > /dev/null && xmakemol -f grid.xyz &" '
+echo 'alias plotcurrent=" $GIMIC_HOME/jobscripts/plot-current-profile.sh" '
+echo 'alias revplotcurrent="gnuplot $GIMIC_HOME/jobscripts/current-profile-plot-reverse.gnu" '
+#echo 'alias gpng="file=$(ls *para.eps) && convert -density 300 $file -resize 1024x1024 $file.png" '
+echo 'alias gsq="$GIMIC_HOME/jobscripts/squares-profile.sh" '
+echo 'alias geps="display *eps &" '
+echo 'alias intprofile="$GIMIC_HOME/jobscripts/intprofile.sh" '
+
+
+echo "function anprofile() { awk -v lower=$1 -v upper=$2 '{ if (($1 >= lower) && ($1 <= upper)) { total+=$2; dia+=$3; para+=$4; } } END { printf("\nTotal current: %f\nDiatropic: %f\nParatropic: %f\n\n", total, dia, para); } ' current_profile.dat ;  }; "
+
+echo 'function dryrun() { gimic --dryrun "$@" > /dev/null ; xmakemol -f grid.xyz;  }; '
