c509764bbfb4345e0e8f3f14c124d053b2ffab52
mariavd <maria.dimitrova@helsinki.fi>
2017-11-04 16:35:06 +0200

    MF is oriented using maximise_projection

--- jobscripts/setup.sh
+++ jobscripts/setup.sh
@@ -2,6 +2,14 @@
 SCRIPTS_IN=$(ls *in)
 SCRIPTS_DIR=$(pwd)
 
+checkMaxProj=$(command -v maximise_projection)
+if [ ! -e $checkMaxProj ]
+then
+    echo "The program maximise_projection is not found."
+    echo "Compile it and export its path."
+    exit
+fi
+
 for file in $SCRIPTS_IN
 do
     SCRIPT_OUT=$(echo ${file/.in/} )
@@ -16,5 +24,6 @@ do
     then 
         sed "$sedstring" $file > $SCRIPT_OUT
         echo "Created script $SCRIPT_OUT."
+	chmod +x $SCRIPT_OUT
     fi
 done
