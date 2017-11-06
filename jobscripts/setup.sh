78608d99b6520cce5e1e068e74f02a52c1d9ace7
mariavd <maria.dimitrova@helsinki.fi>
2017-11-06 18:34:02 +0200

    Bug fixes

--- jobscripts/setup.sh
+++ jobscripts/setup.sh
@@ -37,7 +37,6 @@ echo
 
 echo 'python $GIMIC_HOME/install/bin/turbo2gimic.py > MOL; '
 echo 'alias gim="$GIMIC_HOME/jobscripts/gimic-run.sh" '
-echo 'alias gcurrent="$GIMIC_HOME/jobscripts/current-profile.sh" '
 echo 'alias gmol="python $GIMIC_HOME/jobscripts/turbo2gimic.py > MOL" '
 echo 'alias grid="xmakemol -f grid.xyz &" '
 echo 'alias dgrid="gimic --dryrun bond_integral.inp > /dev/null && xmakemol -f grid.xyz &" '
@@ -47,8 +46,10 @@ echo 'alias revplotcurrent="gnuplot $GIMIC_HOME/jobscripts/current-profile-plot-
 echo 'alias gsq="$GIMIC_HOME/jobscripts/squares-profile.sh" '
 echo 'alias geps="display *eps &" '
 echo 'alias intprofile="$GIMIC_HOME/jobscripts/intprofile.sh" '
-
-
+echo
+echo "Note: on a cluster replace the name of the script below to current-profile-cluster.sh"
+echo 'alias gcurrent="$GIMIC_HOME/jobscripts/current-profile-local.sh" '
+echo 
 echo "function anprofile() { awk -v lower=$1 -v upper=$2 '{ if (($1 >= lower) && ($1 <= upper)) { total+=$2; dia+=$3; para+=$4; } } END { printf("\nTotal current: %f\nDiatropic: %f\nParatropic: %f\n\n", total, dia, para); } ' current_profile.dat ;  }; "
-
+echo
 echo 'function dryrun() { gimic --dryrun "$@" > /dev/null ; xmakemol -f grid.xyz;  }; '
