c4b7d76d6a11f6d419e60c91abe9f522fb831d2d
mariavd <maria.dimitrova@helsinki.fi>
2017-11-17 17:26:12 +0200

    Fixed coord in cluster.sh and 3D density calc

---
+++ jobscripts/3D-run.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+function calcVertices() {
+ awk -v infinity=1000 'BEGIN{
+                         minX=infinity; 
+                         minY=infinity; 
+                         minZ=infinity;  
+                         maxX=-infinity; 
+                         maxY=-infinity; 
+                         maxZ=-infinity
+		         offset=8} 
+                         /\$/{next} {
+                                 if ($1 < minX) {minX = $1;}; 
+                                 if ($2 < minY) {minY = $2;};
+                                 if ($3 < minZ) {minZ = $3;}; 
+                                 if ($1 > maxX) {maxX = $1;}; 
+                                 if ($2 > maxY) {maxY = $2;};
+                                 if ($3 > maxZ) {maxZ = $3;}; 
+                                 }  
+                         END{print minX-offset, minY-offset, minZ-0.5*offset, maxX-minX+2*offset, maxY-minY+2*offset, maxZ-minZ+offset}' coord
+		     }
+                         #END{print minX-8, minY-8, minZ-4, maxX, maxY, maxZ}' coord
+
+
+
+################################################################################################################################
+# DEBUG the min and max:
+  awk -v infinity=1000 'BEGIN{
+                          minX=infinity; 
+                          minY=infinity; 
+                          minZ=infinity;  
+                          maxX=-infinity; 
+                          maxY=-infinity; 
+                          maxZ=-infinity} 
+                          /\$/{next} {
+                                  if ($1 < minX) {minX = $1;}; 
+                                  if ($2 < minY) {minY = $2;};
+                                  if ($3 < minZ) {minZ = $3;}; 
+                                  if ($1 > maxX) {maxX = $1;}; 
+                                  if ($2 > maxY) {maxY = $2;};
+                                  if ($3 > maxZ) {maxZ = $3;}; 
+                                  }  
+                          END{print "Min x, y, z: ", minX, minY, minZ; print "Max x, y, z: ", maxX, maxY, maxZ}' coord
+ 
+
+
+if [ -d 3D ] # Does it exists
+then
+        printf "\n\n*** Directory 3D already exists.\n"
+        echo "Enter [y] to overwrite or any key to exit."; read accept
+        if [ -z $accept ] || [ ! $accept == "y" ]  # if the variable is empty or different from "y", exit
+        then
+            rm -rf 3D # delete it if the user does not want to keep the existing dir
+#        else
+#            mkdir 3D #
+        fi
+else
+    mkdir 3D
+fi
+#printInputFile
+
+#cp /hdd/gimic/jobscripts/gimic.cdens 3D/gimic.inp
+
+vertices=$( calcVertices )
+#echo Vertices: $vertices
+
+vert=(${vertices// / })
+
+#echo MINX = ${vert[0]}
+#echo MINY = ${vert[1]}
+#echo MINZ = ${vert[2]}
+
+
+#echo ${vert[3]}
+#echo ${vert[4]}
+#echo ${vert[5]}
+
+echo "Magnetic field direction"
+# default along the Z axis
+MFx=0.0
+MFy=0.0
+MFz=-1.0
+
+echo "Do you accept the default MF orientation along the Z axis?"
+echo "Press [n] to calculate the direction automatically or [e] to enter manually"
+
+read accept;
+if [ ! -z $accept ] && [ $accept == "n" ]
+then
+   #Bcoords=$(maximise_projection grid.xyz | sed -e 's#{#_#g; s#}#_#g; s#,#_#g' | awk -F [_] '{ {print $2, $3, $4} }')
+   maximise_projection coord.xyz > 3D/field.dat
+   MFx=$( cat 3D/field.dat | sed -e 's#{#_#g; s#}#_#g; s#,#_#g' | awk -F [_] '{ {print -$2} }')
+   MFy=$( cat 3D/field.dat | sed -e 's#{#_#g; s#}#_#g; s#,#_#g' | awk -F [_] '{ {print -$3} }')
+   MFz=$( cat 3D/field.dat | sed -e 's#{#_#g; s#}#_#g; s#,#_#g' | awk -F [_] '{ {print -$4} }')
+fi
+
+echo "Magnetic field vector coordinates: ($MFx; $MFy; $MFz)"
+
+sedstring="s/@MINX@/${vert[0]}/; s/@MINY@/${vert[1]}/; s/@MINZ@/${vert[2]}/; s/@LX@/${vert[3]}/; s/@LY@/${vert[4]}/; s/@LZ@/${vert[5]}/; s/@MFx@/$MFx/; s/@MFy@/$MFy/; s/@MFz@/$MFz/;"
+
+sed "$sedstring" /hdd/gimic/jobscripts/gimic.cdens > 3D/gimic.inp
+
+#x0A=$(awk '/X/{i++}i==1{print $2, $3, $4; exit}' "$wrkdir"/grid.xyz ) 
+#crd0=(${x0A// / })
+#x0x=$(A2bohr ${crd0[0]})
+
