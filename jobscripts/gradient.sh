fc027038cf4cbb8df0b3d0e7473aa643694be674
mariavd <maria.dimitrova@helsinki.fi>
2017-12-13 12:58:16 +0200

    Bug fixes for gradient.sh

--- jobscripts/gradient.sh
+++ jobscripts/gradient.sh
@@ -10,17 +10,12 @@ cat /dev/null > $wrkdir/current.dat
 
 out=$(grep out= $wrkdir/gimic.0.inp | grep -o -E '[0-9.]+')
 start=$(grep in= $wrkdir/gimic.0.inp | grep -o -E '[0-9.]+')
-tmp=$( awk -v start=$start BEGIN'{ print -start }' )
-start=$(echo $tmp) # this is necessary because nowadays width starts with a negative value; in used to be positive
-delta=$( awk -v out=$out -v start=$start 'BEGIN{ value=outr-start; delta=(value<0?-value:value); print delta }' )
-
-echo $out, $start, $delta, $filenum
+delta=$( awk -v out=$out -v start=$start 'BEGIN{ value=out-start; delta=(value<0?-value:value); print delta }' )
 
 echo "Calculating the gradient..."
 
 for (( i=0; i<$filenum; i++ ))
 do
-#    echo "i = $i"
     grep -A 2 "Induced current" $wrkdir/gimic.$i.out | awk -v wrkdir=$wrkdir '{ dia=sprintf("%s/diatropic.dat",wrkdir); para=sprintf("%s/paratropic.dat",wrkdir); if (NR == 2) printf("% f\n", $5) >> dia; else if (NR == 3) printf("% f\n", $5) >> para; }'
     grep "Induced current (nA/T)" $wrkdir/gimic.$i.out | awk -v i=$i -v start=$start -v delta=$delta -v wrkdir=$wrkdir '{ out=sprintf("%s/current.dat",wrkdir); printf("%5.2f\t% f\n", i*delta,$5) >> out; }'
 done
