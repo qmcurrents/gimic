1d9ce214a13d1d3da5bc9dbcb919f19f59849dd4
mariavd <maria.dimitrova@helsinki.fi>
2017-12-17 17:26:48 +0200

    The SLURM parameters are generated from jobscript-header

--- jobscripts/setup.sh
+++ jobscripts/setup.sh
@@ -6,8 +6,8 @@ checkMaxProj=$(command -v maximise_projection)
 if [ ! -e $checkMaxProj ]
 then
     echo "The program maximise_projection is not found."
-    echo "Compile it and export its path."
-    exit
+    echo "Compile it and export its path to use the automatic orientation of the magnetic field."
+#    exit
 fi
 
 for file in $SCRIPTS_IN
@@ -28,6 +28,15 @@ do
     fi
 done
 
+# Prepare the batch job scripts:
+mv jobscript jobscript.tmp
+cat jobscript-header > jobscript
+cat jobscript.tmp >> jobscript
+rm -rf jobscript.tmp
+
+
+echo; echo "REMEMBER TO CHANGE THE BATCH SCRIPT jobscript TO SUIT YOUR CLUSTER"
+
 echo; echo "USEFUL ALIASES AND FUNCTIONS:"; 
 echo "_________________________________________"; echo
 
@@ -35,18 +44,17 @@ echo 'export GIMIC_HOME='${SCRIPTS_DIR/%jobscripts}
 
 echo
 
-echo 'python $GIMIC_HOME/install/bin/turbo2gimic.py > MOL; '
-echo 'alias gim="$GIMIC_HOME/jobscripts/gimic-run.sh" '
-echo 'alias gmol="python $GIMIC_HOME/jobscripts/turbo2gimic.py > MOL" '
+#echo 'python $GIMIC_HOME/install/bin/turbo2gimic.py > MOL; '
+#echo 'alias gmol="python $GIMIC_HOME/jobscripts/turbo2gimic.py > MOL" '
 echo 'alias grid="xmakemol -f grid.xyz &" '
-echo 'alias plotcurrent=" $GIMIC_HOME/jobscripts/plot-current-profile.sh" '
 echo "alias revcurrent=\"mv current_profile.dat current_profile.dat.1 && awk '{printf \"%.6f\t%.6f\t%.6f\t%.6f\n\", \$1, -\$2, -\$4, -\$3}' current_profile.dat.1 > current_profile.dat\" "
-#echo 'alias revplotcurrent="gnuplot $GIMIC_HOME/jobscripts/current-profile-plot-reverse.gnu" '
 #echo 'alias gpng="file=$(ls *para.eps) && convert -density 300 $file -resize 1024x1024 $file.png" '
 #echo 'alias gsq="$GIMIC_HOME/jobscripts/squares-profile.sh" '
+echo 'alias gim="$GIMIC_HOME/jobscripts/gimic-run.sh" '
+echo 'alias 3g="$GIMIC_HOME/jobscripts/3D-run.sh" '
+echo 'alias plotcurrent=" $GIMIC_HOME/jobscripts/plot-current-profile.sh" '
 echo 'alias geps="display *eps &" '
 echo 'alias intprofile="$GIMIC_HOME/jobscripts/intprofile.sh" '
-echo 'alias 3g="$GIMIC_HOME/3D-run.sh" '
 echo
 echo "Note: on a cluster replace the name of the script below to current-profile-cluster.sh"
 echo 'alias gcurrent="$GIMIC_HOME/jobscripts/current-profile-local.sh" '
