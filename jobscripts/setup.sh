a1f1b84b70585270a7b4071f2e5ceccaada99ad8
mariavd <maria.dimitrova@helsinki.fi>
2018-01-21 17:40:49 +0200

    The current-profile-*.in scripts are generated form a common current-profile-header file and the respective *-submit file

--- jobscripts/setup.sh
+++ jobscripts/setup.sh
@@ -10,44 +10,51 @@ then
 #    exit
 fi
 
-for file in $SCRIPTS_IN
-do
-    SCRIPT_OUT=$(echo ${file/.in/} )
-    sedstring="s:@SCRIPTS_DIR@:$SCRIPTS_DIR:"
-    if [ -e $SCRIPT_OUT ]
-    then
-        AGE=$(( $(date -r $file +%s) - $(date -r $SCRIPT_OUT +%s) ))
-    else
-        AGE=1
-    fi
-    if [ "$AGE" -gt 0 ] 
-    then 
-        sed "$sedstring" $file > $SCRIPT_OUT
-        echo "Created script $SCRIPT_OUT."
-	chmod +x $SCRIPT_OUT
-    fi
-done
+
+
+# Prepare the base structure of the current profile script for a local machine:
+file1=current-profile-header
+file2=current-profile-local-submit
+SCRIPT_OUT="current-profile-local.sh.in"
+if [ -e $SCRIPT_OUT ]
+then
+    AGE1=$(( $(date -r $file1 +%s) - $(date -r $SCRIPT_OUT +%s) ))
+    AGE2=$(( $(date -r $file2 +%s) - $(date -r $SCRIPT_OUT +%s) ))
+else
+    AGE1=1
+    AGE2=1
+fi
+if [ "$AGE1" -gt 0 ] || [ "$AGE2" -gt 0 ]
+then 
+    cat current-profile-header > current-profile-local.sh.in
+    cat current-profile-local-submit >> current-profile-local.sh.in
+fi
+
+
+# Prepare the base structure  of the current profile script for cluster
+file1=current-profile-header
+file2=current-profile-cluster-submit
+SCRIPT_OUT="current-profile-cluster.sh.in"
+if [ -e $SCRIPT_OUT ]
+then
+    AGE1=$(( $(date -r $file1 +%s) - $(date -r $SCRIPT_OUT +%s) ))
+    AGE2=$(( $(date -r $file2 +%s) - $(date -r $SCRIPT_OUT +%s) ))
+else
+    AGE1=1
+    AGE2=1
+fi
+if [ "$AGE1" -gt 0 ] || [ "$AGE2" -gt 0 ]
+then 
+    cat current-profile-header > current-profile-cluster.sh.in
+    cat current-profile-cluster-submit >> current-profile-cluster.sh.in
+fi
+
+
+
+
+
 
 # Prepare the batch job scripts:
-# if [ -e jobscript ]
-# then
-#     AGE=$(( $(date -r jobscript.IN +%s) - $(date -r jobscript +%s) ))
-# else 
-#     # jobscript does not exist, so create it
-#     AGE=1
-# fi
-# echo $AGE
-# if [ "$AGE" -gt 0 ] 
-# then 
-#     # jobscript.IN is newer compared to jobscript
-#     sed "$sedstring" $file > $SCRIPT_OUT
-#     echo "Created script $SCRIPT_OUT."
-#     cat jobscript-header > jobscript
-#     cat jobscript.tmp >> jobscript
-#     rm -rf jobscript.tmp
-#     echo; echo "REMEMBER TO CHANGE THE BATCH SCRIPT jobscript TO SUIT YOUR CLUSTER"
-# fi
-
 
 file=jobscript.IN
 SCRIPT_OUT=$(echo ${file/.IN/} )
@@ -66,4 +73,26 @@ then
     echo "Created script $SCRIPT_OUT."
     chmod +x $SCRIPT_OUT
 fi
+
+
+# Prepare all other scripts
+
+for file in $SCRIPTS_IN
+do
+    SCRIPT_OUT=$(echo ${file/.in/} )
+    sedstring="s:@SCRIPTS_DIR@:$SCRIPTS_DIR:"
+    if [ -e $SCRIPT_OUT ]
+    then
+        AGE=$(( $(date -r $file +%s) - $(date -r $SCRIPT_OUT +%s) ))
+    else
+        AGE=1
+    fi
+    if [ "$AGE" -gt 0 ] 
+    then 
+        sed "$sedstring" $file > $SCRIPT_OUT
+        echo "Created script $SCRIPT_OUT."
+	chmod +x $SCRIPT_OUT
+    fi
+done
+
 
