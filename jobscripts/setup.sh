b6191e92ab51b212a68b25e0e94d35fa1904d571
mariavd <maria.dimitrova@helsinki.fi>
2018-01-26 15:15:14 +0200

    Interactive scripts: vertical profile, squares profile, restarting A current profile script for splitting the integration planes in squares
    The squares are now useable for any height of the integration plane
    
    The squares are parallelized correctly for both directions in the local
    script
    
    Bug: maximise_projection program was called when missing. An ! was
    missing
    
    Bug: maximise_projection program was called when missing. An ! was
    missing
    
    Entering a suffix to the calculation directory possible
    
    Bugfix setup.sh
    
    Scripts: Current profiles can now be restarted
    
    Updates to the squares script
    
    More elegant way to run the squares in parallel
    
    A more elegant way to run the squares in parallel
    
    Squares for a cluster
    
    bug fixes for the squares
    
    Squares on a cluster
    
    Bug fix: find instead of ls to avoid "Argument list too long"
    
    Squares: the parallelization is done column-wise; also gradient script
    
    Various bug fixes
    
    Updates to functions-def
    
    Squares are smoothed out
    
    The 3D script asks for spacing as a parameter. The fixed coordinate is more reliable. The grid for the squares can be checked before running the script - it produces the grid.check.xyz file. The squares script creates a subdirectory for each column.
    
    Bug fixes
    
    New vertical profile script; many bug fixes
    
    New vertical profile script; many bug fixes
    
    Vertical script header
    
    Bug fix
    
    Bug fix
    
    Fix the down value sign problem
    
    Bug fixes in 3D and current profile header
    
    Setup was not checking for changes in the batch job header file
    
    Bug fixes; fixed coord from index; using Raul's plane program
    
    Bug fixes in the updated current plot and the MF direction with Raul's program
    
    Bash aliases updated
    
    Changed the angle of rotation for the fixed point
    
    Bug fixes
    
    Bugfix

--- jobscripts/setup.sh
+++ jobscripts/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPTS_DIR=$(pwd)
+
 checkMaxProj=$(command -v maximise_projection)
 if [ ! -e $checkMaxProj ]
 then
@@ -19,52 +21,72 @@ fi
 # Prepare the base structure of the squares profile script for a local machine:
 file1="src/squares-profile-header"
 file2="src/squares-profile-local-submit"
-file3="src/functions-def"
 SCRIPT_OUT="src/squares-profile-local.sh.in"
 
-if [ $SCRIPT_OUT -ot $file1 ] || [ $SCRIPT_OUT -ot $file2 ] || [ $SCRIPT_OUT -ot $file3 ]  # [ FILE1 -ot FILE2 ]  -> True if FILE1 is older than FILE2, or is FILE2 exists and FILE1 does not.
+if [ $SCRIPT_OUT -ot $file1 ] || [ $SCRIPT_OUT -ot $file2 ]  # [ FILE1 -ot FILE2 ]  -> True if FILE1 is older than FILE2, or is FILE2 exists and FILE1 does not.
 then 
-    cat src/squares-profile-header > src/squares-profile-local.sh.in
-    cat src/squares-profile-local-submit >> src/squares-profile-local.sh.in
+    cat src/squares-profile-header > $SCRIPT_OUT
+    cat src/squares-profile-local-submit >> $SCRIPT_OUT
 fi
+
+# Prepare the base structure of the vertical profile script for a local machine:
+file1="src/vertical-profile-header"
+file2="src/current-profile-local-submit"
+SCRIPT_OUT="src/vertical-profile-local.sh.in"
+
+if [ $SCRIPT_OUT -ot $file1 ] || [ $SCRIPT_OUT -ot $file2 ]  
+then 
+    cat src/vertical-profile-header > $SCRIPT_OUT
+    cat src/current-profile-local-submit >> $SCRIPT_OUT
+fi
+
 
 # Prepare the base structure of the current profile script for a local machine:
 file1="src/current-profile-header"
 file2="src/current-profile-local-submit"
-file3="src/functions-def"
 SCRIPT_OUT="src/current-profile-local.sh.in"
 
-if [ $SCRIPT_OUT -ot $file1 ] || [ $SCRIPT_OUT -ot $file2 ] || [ $SCRIPT_OUT -ot $file3 ]  # [ FILE1 -ot FILE2 ]  -> True if FILE1 is older than FILE2, or is FILE2 exists and FILE1 does not.
+if [ $SCRIPT_OUT -ot $file1 ] || [ $SCRIPT_OUT -ot $file2 ]
 then 
-    cat src/current-profile-header > src/current-profile-local.sh.in
-    cat src/current-profile-local-submit >> src/current-profile-local.sh.in
+    cat src/current-profile-header > $SCRIPT_OUT
+    cat src/current-profile-local-submit >> $SCRIPT_OUT
 fi
 
 # Prepare the base structure  of the squares profile script for cluster
 
 file1="src/squares-profile-header"
 file2="src/squares-profile-cluster-submit"
-file3="src/functions-def"
 SCRIPT_OUT="src/squares-profile-cluster.sh.in"
 
-if [ $SCRIPT_OUT -ot $file1 ] || [ $SCRIPT_OUT -ot $file2 ] || [ $SCRIPT_OUT -ot $file3 ]
+if [ $SCRIPT_OUT -ot $file1 ] || [ $SCRIPT_OUT -ot $file2 ] 
 then 
-    cat src/squares-profile-header > src/squares-profile-cluster.sh.in
-    cat src/squares-profile-cluster-submit >> src/squares-profile-cluster.sh.in
+    cat src/squares-profile-header > $SCRIPT_OUT
+    cat src/squares-profile-cluster-submit >> $SCRIPT_OUT
 fi
-
 
 # Prepare the base structure  of the current profile script for cluster
 
 file1="src/current-profile-header"
 file2="src/current-profile-cluster-submit"
-file3="src/functions-def"
 SCRIPT_OUT="src/current-profile-cluster.sh.in"
 
-if [ $SCRIPT_OUT -ot $file1 ] || [ $SCRIPT_OUT -ot $file2 ] || [ $SCRIPT_OUT -ot $file3 ]
+if [ $SCRIPT_OUT -ot $file1 ] || [ $SCRIPT_OUT -ot $file2 ] 
 then 
-    cat src/current-profile-header > src/current-profile-cluster.sh.in
-    cat src/current-profile-cluster-submit >> src/current-profile-cluster.sh.in
+    cat src/current-profile-header > $SCRIPT_OUT
+    cat src/current-profile-cluster-submit >> $SCRIPT_OUT
+fi
+
+
+# Prepare the base structure  of the vertical profile script for cluster
+
+file1="src/vertical-profile-header"
+file2="src/current-profile-cluster-submit"
+SCRIPT_OUT="src/vertical-profile-cluster.sh.in"
+
+if [ $SCRIPT_OUT -ot $file1 ] || [ $SCRIPT_OUT -ot $file2 ] 
+then 
+    cat src/vertical-profile-header > $SCRIPT_OUT
+    cat src/current-profile-cluster-submit >> $SCRIPT_OUT
 fi
 
 
@@ -73,7 +95,7 @@ fi
 file="src/jobscript.IN"
 SCRIPT_OUT=jobscript
 sedstring="s:@SCRIPTS_DIR@:$SCRIPTS_DIR:"
-if [ $SCRIPT_OUT -ot $file ] 
+if [ $SCRIPT_OUT -ot $file ] || [ $SCRIPT_OUT -ot src/jobscript-header ]
 then 
     echo; echo "REMEMBER TO CHANGE THE BATCH SCRIPT jobscript-header TO SUIT YOUR CLUSTER BEFORE SETUP"
     cat src/jobscript-header > $SCRIPT_OUT
@@ -84,10 +106,10 @@ fi
 
 # Prepare the batch job script for squares profile:
 
-file="src/jobscript.IN"
+file="src/jobscript-squares.IN"
 SCRIPT_OUT=jobscript-squares
 sedstring="s:@SCRIPTS_DIR@:$SCRIPTS_DIR:"
-if [ $SCRIPT_OUT -ot $file ] 
+if [ $SCRIPT_OUT -ot $file ] || [ $SCRIPT_OUT -ot src/jobscript-header ]
 then 
     cat src/jobscript-header > $SCRIPT_OUT
     sed "$sedstring" $file >> $SCRIPT_OUT
@@ -98,7 +120,6 @@ fi
 
 # Prepare all other scripts
 SCRIPTS_IN=$(ls src/*in)
-SCRIPTS_DIR=$(pwd)
 
 for file in $SCRIPTS_IN
 do
--- src/fgimic/integral.f90
+++ src/fgimic/integral.f90
@@ -44,6 +44,7 @@ contains
 
     subroutine del_integral(this)
         type(integral_t) :: this
+        nullify(this%grid)
     end subroutine
 
     subroutine integrate_current(this, mol, xdens, spinn)
@@ -53,23 +54,12 @@ contains
         character(*), optional :: spinn
 
         integer(I4) :: i, j, k, p1, p2, p3, lo, hi
-        real(DP), dimension(3) :: normal, rr, center, bb, bb_prime, bb_second
+        real(DP), dimension(3) :: normal, rr, center, bb
         real(DP) :: psum, nsum, w, jp, r, bound
         real(DP) :: psum2, nsum2
         real(DP) :: psum3, nsum3
         real(DP) :: xsum, xsum2, xsum3
-
-        real(DP) :: jp_prime, jp_second
-        real(DP) :: p_prime_sum, n_prime_sum
-        real(DP) :: p_prime_sum2, n_prime_sum2
-        real(DP) :: p_prime_sum3, n_prime_sum3
-        real(DP) :: x_prime_sum, x_prime_sum2, x_prime_sum3
-        real(DP) :: p_second_sum, n_second_sum
-        real(DP) :: p_second_sum2, n_second_sum2
-        real(DP) :: p_second_sum3, n_second_sum3
-        real(DP) :: x_second_sum, x_second_sum2, x_second_sum3
-
-        real(DP), dimension(3) :: jvec, jvec_prime, jvec_second
+        real(DP), dimension(3) :: jvec
         real(DP), dimension(9) :: tt
         type(jtensor_t) :: jt
 
@@ -97,9 +87,6 @@ contains
 
         normal=get_grid_normal(this%grid)
 
-        bb_prime=normal
-        bb_second=cross_product(bb,bb_prime);
-
         bound=1.d+10
         bound=this%grid%radius
         if (bound < 1.d+10) then
@@ -117,169 +104,83 @@ contains
         psum3=0.d0
         nsum3=0.d0
 
-        x_prime_sum3=0.d0
-        p_prime_sum3=0.d0
-        n_prime_sum3=0.d0
-        x_second_sum3=0.d0
-        p_second_sum3=0.d0
-        n_second_sum3=0.d0
-!!$OMP PARALLEL PRIVATE(i,j,k,r,rr,xsum,psum,nsum) &
-!!$OMP PRIVATE(jt,w,jp,tt,jvec) &
-!!$OMP SHARED(xsum2,psum2,nsum2) &
-!!$OMP SHARED(p1,p2,p3,this,center,spin,bb,normal,mol,xdens,lo,hi) &
-!!$OMP REDUCTION(+:xsum3,psum3,nsum3)
+!$OMP PARALLEL DEFAULT(NONE) &
+!$OMP PRIVATE(i,j,k,r,rr,xsum,psum,nsum) &
+!$OMP PRIVATE(jt,w,jp,tt,jvec) &
+!$OMP SHARED(p1,p2,p3,this,center,spin,bb,normal,mol,xdens,lo,hi,bound) &
+!$OMP REDUCTION(+:xsum3,psum3,nsum3,xsum2,psum2,nsum2)
         call new_jtensor(jt, mol, xdens)
         do k=1,p3
             xsum2=0.d0
             psum2=0.d0
             nsum2=0.d0
 
-            x_prime_sum2=0.d0
-            p_prime_sum2=0.d0
-            n_prime_sum2=0.d0
-            x_second_sum2=0.d0
-            p_second_sum2=0.d0
-            n_second_sum2=0.d0
-
-            !!$OMP DO SCHEDULE(STATIC) REDUCTION(+:xsum2,psum2,nsum2)
+            !$OMP DO
             do j=lo,hi
                 xsum=0.d0
                 psum=0.d0
                 nsum=0.d0
-
-                x_prime_sum=0.d0
-                p_prime_sum=0.d0
-                n_prime_sum=0.d0
-                x_second_sum=0.d0
-                p_second_sum=0.d0
-                n_second_sum=0.d0
-
                 do i=1,p1
                     rr=gridpoint(this%grid, i, j, k)
                     r=sqrt(sum((rr-center)**2))
                     call ctensor(jt, rr, tt, spin)
                     jvec=matmul(reshape(tt,(/3,3/)),bb)
-                    jvec_prime=matmul(reshape(tt,(/3,3/)),bb_prime)
-                    jvec_second=matmul(reshape(tt,(/3,3/)),bb_second)
                     if ( r > bound ) then
                         w=0.d0
                         jp=0.d0
-                        jp_prime=0.d0
-                        jp_second=0.d0
                     else
                         w=get_weight(this%grid, i, 1)
                         jp=dot_product(normal,jvec)*w
-                        jp_prime=dot_product(normal,jvec_prime)*w
-                        jp_second=dot_product(normal,jvec_second)*w
                     end if
                     ! total
                     xsum=xsum+jp
-                    x_prime_sum = x_prime_sum + jp_prime
-                    x_second_sum = x_second_sum + jp_second
                     if (jp > 0.d0) then
                         ! get positive contribution
                         psum=psum+jp
-                        p_prime_sum = p_prime_sum + jp_prime
-                        p_second_sum = p_second_sum + jp_second
-
                     else
                         ! get negative contribution
                         nsum=nsum+jp
-                        n_prime_sum = n_prime_sum + jp_prime
-                        n_second_sum = n_second_sum + jp_second
                     end if
-                    end do
+                end do
                 w=get_weight(this%grid,j,2)
                 xsum2=xsum2+xsum*w
                 psum2=psum2+psum*w
                 nsum2=nsum2+nsum*w
-                
-                x_prime_sum2=x_prime_sum2+x_prime_sum*w
-                p_prime_sum2=p_prime_sum2+p_prime_sum*w
-                n_prime_sum2=n_prime_sum2+n_prime_sum*w
-
-                x_second_sum2=x_second_sum2+x_second_sum*w
-                p_second_sum2=p_second_sum2+p_second_sum*w
-                n_second_sum2=n_second_sum2+n_second_sum*w
-                
             end do
-            !!$OMP END DO
-            !!$OMP MASTER
-            call collect_sum(xsum2, xsum)
-            call collect_sum(psum2, psum)
-            call collect_sum(nsum2, nsum)
-            w=get_weight(this%grid,k,3)
-            xsum3=xsum3+xsum*w
-            psum3=psum3+psum*w
-            nsum3=nsum3+nsum*w
+            !$OMP END DO
 
-            x_prime_sum3=x_prime_sum3+x_prime_sum*w
-            p_prime_sum3=p_prime_sum3+p_prime_sum*w
-            n_prime_sum3=n_prime_sum3+n_prime_sum*w
+            w = get_weight(this%grid,k,3)
+            xsum3 = xsum3 + w*xsum2
+            psum3 = psum3 + w*psum2
+            nsum3 = nsum3 + w*nsum2
 
-            x_second_sum3=x_second_sum3+x_second_sum*w
-            p_second_sum3=p_second_sum3+p_second_sum*w
-            n_second_sum3=n_second_sum3+n_second_sum*w
-            !!$OMP END MASTER
+            ! TODO old code used collect_sum
+            ! this will be needed for MPI
+!           call collect_sum(xsum2, xsum)
+!           call collect_sum(psum2, psum)
+!           call collect_sum(nsum2, nsum)
+
         end do
         call del_jtensor(jt)
-!!$OMP END PARALLEL
-
-write (*,*) 'DEBUGGING'
+!$OMP END PARALLEL
 
         call nl
         call msg_out(repeat('*', 60))
-!        write(str_g, '(a,3f10.5)') '   Magnetic field <x,y,z> =', bb
-!        call msg_out(str_g)
-        write(str_g, '(a,f15.8)') '   Induced current (au)    :', xsum3
+        write(str_g, '(a,f13.6)') '   Induced current (au)    :', xsum3
         call msg_out(str_g)
-        write(str_g, '(a,f15.8,a,f13.8,a)') &
+        write(str_g, '(a,f13.6,a,f11.6,a)') &
             '      Positive contribution:', psum3, '  (',au2si(psum3),' )'
         call msg_out(str_g)
-        write(str_g, '(a,f15.8,a,f13.8,a)') &
+        write(str_g, '(a,f13.6,a,f11.6,a)') &
             '      Negative contribution:', nsum3, '  (',au2si(nsum3),' )'
         call msg_out(str_g)
         call nl
-        write(str_g, '(a,f15.8)') '   Induced current (nA/T)  :', au2si(xsum3)
+        write(str_g, '(a,f13.6)') '   Induced current (nA/T)  :', au2si(xsum3)
         call msg_out(str_g)
-        write(str_g, '(a,f15.8)') '      (conversion factor)  :', au2si(1.d0)
+        write(str_g, '(a,f13.6)') '      (conversion factor)  :', au2si(1.d0)
         call msg_out(str_g)
-        call nl
-
-        call nl
         call msg_out(repeat('*', 60))
         call nl
-        write(str_g, '(a,3f10.5)') '   Magnetic field <x,y,z> =', bb_prime
-        call msg_out(str_g)
-        write(str_g, '(a,f15.8)') '   Induced j_prime current (au)    :', x_prime_sum3
-        call msg_out(str_g)
-        write(str_g, '(a,f15.8,a,f13.8,a)') &
-            '      Positive j_prime contribution:', p_prime_sum3, '  (',au2si(p_prime_sum3),' )'
-        call msg_out(str_g)
-        write(str_g, '(a,f15.8,a,f13.8,a)') &
-            '      Negative j_prime contribution:', n_prime_sum3, '  (',au2si(n_prime_sum3),' )'
-        call msg_out(str_g)
-        call nl
-        write(str_g, '(a,f15.8)') '   Induced j_prime current (nA/T)  :', au2si(x_prime_sum3)
-        call msg_out(str_g)
-        call nl
-        call msg_out(repeat('*', 60))
-        call nl
-        write(str_g, '(a,3f10.5)') '   Magnetic field <x,y,z> =', bb_second
-        call msg_out(str_g)
-        write(str_g, '(a,f15.8)') '   Induced j_second current (au)    :', x_second_sum3
-        call msg_out(str_g)
-        write(str_g, '(a,f15.8,a,f13.8,a)') &
-            '      Positive j_second contribution:', p_second_sum3, '  (',au2si(p_second_sum3),' )'
-        call msg_out(str_g)
-        write(str_g, '(a,f15.8,a,f13.8,a)') &
-            '      Negative j_second contribution:', n_second_sum3, '  (',au2si(n_second_sum3),' )'
-        call msg_out(str_g)
-        call nl
-        write(str_g, '(a,f15.8)') '   Induced j_second current (nA/T)  :', au2si(x_second_sum3)
-        call msg_out(str_g)
-        call nl
-        call msg_out(repeat('*', 60))
 
         spin = 'total'
     end subroutine
@@ -340,17 +241,19 @@ write (*,*) 'DEBUGGING'
         xsum3=0.d0
         psum3=0.d0
         nsum3=0.d0
-!$OMP PARALLEL PRIVATE(i,j,k,r,rr,sgn,xsum,psum,nsum) &
+
+!$OMP PARALLEL DEFAULT(NONE) &
+!$OMP PRIVATE(i,j,k,r,rr,sgn,xsum,psum,nsum) &
 !$OMP PRIVATE(jt,w,jp,tt,jvec) &
-!$OMP SHARED(xsum2,psum2,nsum2) &
-!$OMP SHARED(p1,p2,p3,this,center,spin,bb,normal,lo,hi) &
-!$OMP REDUCTION(+:xsum3,psum3,nsum3)
+!$OMP SHARED(p1,p2,p3,this,center,spin,bb,normal,lo,hi,bound,xdens,mol) &
+!$OMP REDUCTION(+:xsum3,psum3,nsum3,xsum2,psum2,nsum2)
         call new_jtensor(jt, mol, xdens)
         do k=1,p3
             xsum2=0.d0
             psum2=0.d0
             nsum2=0.d0
-            !$OMP DO SCHEDULE(STATIC) REDUCTION(+:xsum2,psum2,nsum2)
+
+            !$OMP DO
             do j=lo,hi
                 xsum=0.d0
                 psum=0.d0
@@ -387,15 +290,15 @@ write (*,*) 'DEBUGGING'
                 nsum2=nsum2+nsum*w
             end do
             !$OMP END DO
-            !$OMP MASTER
-            call collect_sum(xsum2, xsum)
-            call collect_sum(psum2, psum)
-            call collect_sum(nsum2, nsum)
-            w=get_weight(this%grid,k,3)
-            xsum3=xsum3+xsum*w
-            psum3=psum3+psum*w
-            nsum3=nsum3+nsum*w
-            !$OMP END MASTER
+
+            w = get_weight(this%grid,k,3)
+            xsum3 = xsum3 + w*xsum2
+            psum3 = psum3 + w*psum2
+            nsum3 = nsum3 + w*nsum2
+
+            ! TODO old code used collect_sum
+            ! this will be needed for MPI
+
         end do
         call del_jtensor(jt)
 !$OMP END PARALLEL
@@ -555,16 +458,17 @@ write (*,*) 'DEBUGGING'
         end if
 
         xsum3=0.d0
-! NB! Untested parallelization, check with valgrind!
-!$OMP PARALLEL PRIVATE(i,j,k,r,rr,xsum) &
-!$OMP PRIVATE(jt,w,tt) &
-!$OMP SHARED(xsum2) &
-!$OMP SHARED(p1,p2,p3,this,center,spin,mol,xdens,lo,hi) &
-!$OMP REDUCTION(+:xsum3)
+
+!$OMP PARALLEL DEFAULT(NONE) &
+!$OMP PRIVATE(i,j,k,r,rr,xsum) &
+!$OMP PRIVATE(jt,w,tt,val) &
+!$OMP SHARED(p1,p2,p3,this,center,spin,mol,xdens,lo,hi,bound) &
+!$OMP REDUCTION(+:xsum3,xsum2)
         call new_jtensor(jt, mol, xdens)
         do k=1,p3
             xsum2=0.d0
-            !$OMP DO SCHEDULE(STATIC) REDUCTION(+:xsum2)
+
+            !$OMP DO
             do j=lo,hi
                 xsum=0.d0
                 do i=1,p1
@@ -572,7 +476,7 @@ write (*,*) 'DEBUGGING'
                     r=sqrt(sum((rr-center)**2))
                     call ctensor(jt, rr, tt, spin)
                     ! attention: output of get_acid is in au !
-                    val = get_acid(rr,tt)
+                    val = get_acid(tt)
                     if ( r > bound ) then
                         w=0.d0
                     else
@@ -584,15 +488,16 @@ write (*,*) 'DEBUGGING'
                 xsum2=xsum2+xsum*w
             end do
             !$OMP END DO
-            !$OMP MASTER
-            call collect_sum(xsum2, xsum)
-            w=get_weight(this%grid,k,3)
-            xsum3=xsum3+xsum*w
-            !$OMP END MASTER
+
+            w = get_weight(this%grid,k,3)
+            xsum3 = xsum3 + w*xsum2
+
+            ! TODO old code used collect_sum
+            ! this will be needed for MPI
         end do
         call del_jtensor(jt)
-        acid_val = dsqrt(xsum3)
 !$OMP END PARALLEL
+        acid_val = dsqrt(xsum3)
 
         call nl
         call msg_out(repeat('*', 60))
