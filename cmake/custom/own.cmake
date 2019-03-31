b7fa015b8ac6f5d0ae158dd11eb9a964b86f1d14
Radovan Bast <bast@users.noreply.github.com>
2019-03-31 18:46:22 +0200

    rm more unused code

--- cmake/custom/own.cmake
+++ cmake/custom/own.cmake
@@ -9,7 +9,6 @@ set(PYTHON_SITE_INSTALL_DIR
     lib/python${PYTHON_VERSION}/site-packages/gimic)
 
 include(cython)
-include(testing)
 
 configure_file(
     ${PROJECT_SOURCE_DIR}/config.h.in
@@ -17,4 +16,6 @@ configure_file(
     )
 
 add_subdirectory(tools)
+
+enable_testing()
 add_subdirectory(test)
